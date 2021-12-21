ec1cc590ebecef21389612bfa2e6cfb3dba74972
Ivanilson França Vieira Junior <ivanilsonjunior@gmail.com>
2021-12-21 10:06:34 +0000

    Update run.me
    The first pass is to install the requirements.

--- runMe.sh
+++ runMe.sh
@@ -1,5 +1,8 @@
 # runMe - Configs
 #
-# 1 - We use the viewconf tool to get Environment so we need to put these lines on the end
+# 1 - Install requisites via pip
+sudo pip install -r requirements.txt
+#
+# 2 - We use the viewconf tool to get Environment so we need to put these lines on the end
 echo \#\#\#\#\# \"APP_SEND_INTERVAL_SEC\": _________________ == APP_SEND_INTERVAL_SEC >> ../../tools/viewconf/viewconf.c
 echo \#\#\#\#\# \"APP_WARM_UP_PERIOD_SEC\": ________________ == APP_WARM_UP_PERIOD_SEC >> ../../tools/viewconf/viewconf.c
