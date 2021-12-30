fd76700f34c1b0dcca7ff7b33fe2f4aa835d6fbe
Ivanilson Junior <ivanilsonjunior@gmail.com>
2021-12-30 08:18:41 +0000

    Change default port to 9001

--- api.py
+++ api.py
@@ -115,4 +115,4 @@ def summaryRun(id):
     return render_template("runSummary.html", run=run)
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", debug=True)
+    app.run(host="0.0.0.0", port=9001, debug=True)
